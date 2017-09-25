169d7d07f55fe458c210f59c87e68f4d97bffacd
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-25 20:26:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,12 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     if phrase == "I LOVE YOU GRANDMA!"
-      puts 'I LOVE YOU TOO PUMPKIN!'
+      return 'I LOVE YOU TOO PUMPKIN!'
     else
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     end
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
